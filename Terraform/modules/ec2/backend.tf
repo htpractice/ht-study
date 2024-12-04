fbe488ed9304b0d4a672eb451088fad2ce900441
harshal.thaware <harshal.thaware@oracle.com>
2024-12-04 07:32:42 +0530

    dockerfile example

--- Terraform/modules/ec2/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-      region = "us"
-      bucket = "<bucket name>"
-      key = "<path to tfstate in S3>"
-      dynamodb_table = "<hcl lock table name>"
-  }
-}
--- Terraform/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-ami = "i-72373429284d"
-aws_ec2_host_type = "t2.micro"
-subnet_id = "i-scjssc345d"
